bef9cb54131a11db38421faa2bd6cf669aee60a8
Matheus Leal <matheusleal1301@gmail.com>
2024-12-12 09:16:24 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,15 @@
+// model compreende não apenas as entidades, mas também as classes que fazem transformações nessas entidades
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
+}
